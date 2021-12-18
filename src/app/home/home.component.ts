63acd2c2072ffc8c0270a49abe100567bf5928b6
SiewLeng <siewleng.lim@gmail.com>
2021-12-18 18:18:14 +0800

    add content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,11 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+   public tabs: any = [
+     {img: '../../assets/images/health-record-icon.png', title: 'COVID-19 RECORDS'},
+     {img: '../../assets/images/health-record-icon.png', title: 'PERSONAL HEALTH'},
+     {img: '../../assets/images/health-record-icon.png', title: 'APPOINTMENTS'},
+     {img: '../../assets/images/health-record-icon.png', title: 'PAYMENTS'},
+     {img: '../../assets/images/health-record-icon.png', title: 'MEDICATION REFILL'},
+     {img: '../../assets/images/health-record-icon.png', title: 'ADMISSIONS'}
+   ];
 
   constructor() { }
 
   ngOnInit() {
     console.log('Home page');
+  }
+
+  newAppointment() {
+    console.log('Make new appointment');
   }
 
 }
